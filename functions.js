e797990744b2cdf7c53cd49574545c9b98801e64
Esh <eshesh594@gmail.com>
2022-11-05 23:58:39 +0000

    add: initial function defination

---
+++ functions.js
@@ -0,0 +1,10 @@
+// the way to declare and define the function is
+// by "function" keyword follow by the name of function
+
+/*
+* "function"
+*
+*/
+function square(number) {
+    return number * number;
+}
